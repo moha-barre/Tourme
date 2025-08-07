696efebf524e68df1ed5f687f6548712230a1024
moha-barre <mobarre001@gmail.com>
2025-08-07 19:29:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   base: "/",
-  build:{
-   outDir: "dist",
-}
+  
 });
