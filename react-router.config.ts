05679dda5839a35ef892e9580814e91a118111cc
moha-barre <mobarre001@gmail.com>
2025-08-07 19:19:38 +0300

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
   // Add server entry point for SSR
   serverEntryPoint: "./app/entry.server.tsx",
 } satisfies Config;
